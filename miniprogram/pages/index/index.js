ae54a72f1d9352abdebc67219ba09544ba45f178
unknown <unknown@example.com>
2019-10-10 11:50:49 +0800

    List

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,18 +1,24 @@
-// pages/index/index.js
+const db = wx.cloud.database();
+const todos = db.collection('todos');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tasks:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    todos.get().then(res =>{
+      console.log()
+      this.setData({
+        tasks:res.data
+      })
+    })
   },
 
   /**
@@ -26,41 +32,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
